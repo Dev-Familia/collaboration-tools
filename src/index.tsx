d72868922f014cd167d5aeeef197b6bddfb14c73
xollaborator <54781638+xollaborator@users.noreply.github.com>
2022-05-29 00:24:21 +0200

    build: turn off capitalized-comments rule in eslint

--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,7 @@ import ReactDOM from 'react-dom/client'
 
 import App from './App'
 
-const theme = extendTheme({ shadows: { outline: '0 0 0 3px #9F7AEA' } }) // Color: purple.400
+const theme = extendTheme({ shadows: { outline: '0 0 0 3px #9F7AEA' } }) // color: purple.400
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
